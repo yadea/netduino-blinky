443acb1001e4a5747f9e14249934ed875059bd0e
yadea <kuanyiaun@hotmail.com>
2016-11-10 01:30:37 +0800

    Added press to off LED.

--- blinky/Program.cs
+++ blinky/Program.cs
@@ -12,13 +12,17 @@ namespace blinky
         public static void Main()
         {
             OutputPort led = new OutputPort(Pins.ONBOARD_LED, false);
+            InputPort button = new InputPort(Pins.ONBOARD_SW1, false, Port.ResistorMode.Disabled);
+            bool buttonState = false;
 
             while(true)
             {
-                led.Write(true);
-                Thread.Sleep(250);
-                led.Write(false);
-                Thread.Sleep(250);
+                buttonState = button.Read();
+                led.Write(buttonState);
+                //led.Write(true);
+                //Thread.Sleep(250);
+                //led.Write(false);
+                //Thread.Sleep(250);
             }
         }
 
