1ce0f460b752781bb2454a62779ba383718abe1c
yadea <kuanyiaun@hotmail.com>
2016-11-16 00:46:17 +0800

    Added MakerShield project. Set firmware to version 4.2.

---
+++ 2. MakerShield/MakerShield/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace MakerShield
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // write your code here
+            OutputPort led = new OutputPort(Pins.GPIO_PIN_D0, false);
+            InputPort button = new InputPort(Pins.GPIO_PIN_D1, false, Port.ResistorMode.PullUp);
+            bool buttonState = false;
+
+            while (true)
+            {
+                buttonState = !button.Read();
+                led.Write(buttonState);
+            }
+        }
+
+    }
+}
