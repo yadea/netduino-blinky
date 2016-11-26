8da91ea24733735edcd83d79ff6c1cbfb8cfed91
yadea <kuanyiaun@hotmail.com>
2016-11-27 01:11:35 +0800

    Set to blink based on potentiometer

--- 2. MakerShield/MakerShield/Program.cs
+++ 2. MakerShield/MakerShield/Program.cs
@@ -11,8 +11,8 @@ namespace MakerShield
     {
         public static void Main()
         {
-            // write your code here
-            OutputPort led = new OutputPort(Pins.GPIO_PIN_D0, false);
+            // Toggle light when shield button is pressed.
+            /*OutputPort led = new OutputPort(Pins.GPIO_PIN_D0, false);
             InputPort button = new InputPort(Pins.GPIO_PIN_D1, false, Port.ResistorMode.PullUp);
             bool buttonState = false;
 
@@ -20,8 +20,26 @@ namespace MakerShield
             {
                 buttonState = !button.Read();
                 led.Write(buttonState);
+            }*/
+
+            // Measure voltage from potentiometer
+            OutputPort led = new OutputPort(Pins.GPIO_PIN_D0, false);
+            var pot = new SecretLabs.NETMF.Hardware.AnalogInput(Pins.GPIO_PIN_A0); //need to inport SecretLabs.NETMF.Hardware.AnalogInput reference.
+
+            int potValue = 0;
+
+            while(true)
+            {
+                //read the potentiometer value
+                potValue = pot.Read();
+                Debug.Print(potValue.ToString());
+
+                //blink value based on value (0-1023 ms)
+                led.Write(true);
+                Thread.Sleep(potValue);
+                led.Write(false);
+                Thread.Sleep(potValue);
             }
         }
-
     }
 }
